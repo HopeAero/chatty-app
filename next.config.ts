979e57556923b8fd087e0b16a556571ba73f4b75
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2025-01-16 21:03:10 -0400

    Actualizar configuración de Next.js: agregar variables de entorno y modificar la estructura de fuentes

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "upgrade-insecure-requests"
-          }
-        ],
-      },
-    ]
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_GATEWAY_URL: process.env.NEXT_PUBLIC_GATEWAY_URL,
   },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/:path*',
+  //       headers: [
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: "upgrade-insecure-requests"
+  //         }
+  //       ],
+  //     },
+  //   ]
+  // },
 };
 
 export default nextConfig;
